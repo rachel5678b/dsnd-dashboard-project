71f38f27a791ef3eb659a9778b333ebe99d780bd
Rachel Bieron <rbieron@mtb,com>
2025-06-11 20:09:43 -0400

    updated code

--- python-package/employee_events/query_base.py
+++ python-package/employee_events/query_base.py
@@ -1,27 +1,27 @@
 # Import any dependencies needed to execute sql queries
-# YOUR CODE HERE
+from sql_execution import QueryMixin
 
 # Define a class called QueryBase
 # Use inheritance to add methods
 # for querying the employee_events database.
-# YOUR CODE HERE
+class QueryBase(QueryMixin):
 
     # Create a class attribute called `name`
     # set the attribute to an empty string
-    # YOUR CODE HERE
+    name = ""
 
     # Define a `names` method that receives
     # no passed arguments
-    # YOUR CODE HERE
+    def names(self):
         
         # Return an empty list
-        # YOUR CODE HERE
+        return []
 
 
     # Define an `event_counts` method
     # that receives an `id` argument
     # This method should return a pandas dataframe
-    # YOUR CODE HERE
+    def event_counts(self, id):
 
         # QUERY 1
         # Write an SQL query that groups by `event_date`
@@ -31,13 +31,25 @@
         # Use f-string formatting to set the name
         # of id columns used for joining
         # order by the event_date column
-        # YOUR CODE HERE
-            
+        sql_query = f"""
+            SELECT event_date,
+                   SUM(positive_events) AS total_positive_events,
+                   SUM(negative_events) AS total_negative_events
+            FROM {self.name}
+            JOIN employee_events
+                USING ({self.name}_id)
+            WHERE {self.name}.{self.name}_id = employee_events.{self.name}_id 
+            GROUP BY event_date
+            ORDER BY event_date;
+        """
+        events_df = self.pandas_query(sql_query)
     
+        return events_df
 
+            
     # Define a `notes` method that receives an id argument
     # This function should return a pandas dataframe
-    # YOUR CODE HERE
+    def notes(self, id):
 
         # QUERY 2
         # Write an SQL query that returns `note_date`, and `note`
@@ -46,5 +58,13 @@
         # with f-string formatting
         # so the query returns the notes
         # for the table name in the `name` class attribute
-        # YOUR CODE HERE
-
+        sql_query = f"""
+            SELECT note_date, note
+            FROM notes
+            JOIN {self.name} 
+                USING ({self.name}_id) 
+            WHERE notes.{self.name}_id = {self.name}.{self.name}_id;
+        """
+        notes_df = self.pandas_query(sql_query)
+    
+        return notes_df
